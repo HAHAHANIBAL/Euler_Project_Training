ca3609f91246bc1d48498f04499fed58ce939929
Mo Chen <kidrock2006@gmail.com>
2014-02-18 04:25:48 -0500

    Updates
    1. Modified Euler 37

--- Truncatable primes.py
+++ Truncatable primes.py
@@ -5,12 +5,15 @@
 
 import sympy
 #can be further reduce to [1,2,3,5,7,9] and [3,7]
-appends=[1,2,3,4,5,6,7,8,9]
-candidate=[2,3,5,7]
+#in the prime number 2 and 5 are impossible for right hand truncate
+#in the append list 4,6,8 are impossible because they are (1) not primes and (2) are divisible for right hand truncate
+appends=[1,2,3,5,7,9]
+candidate=[3,7]
 sum=0
 count=0
 while count<11:
     prime=candidate[0]
+    #pop out the first elements of the lists
     candidate.remove(prime)
     if sympy.isprime(prime):
         istruncate=True
@@ -22,8 +25,9 @@ while count<11:
             multiplier*=10
         if istruncate and prime>10:
             sum+=prime
+            #loop termination control
             count+=1
-            print prime
         for i in range(0,len(appends)):
+            #generate the new candidate lists and loop again
             candidate.append(multiplier*appends[i]+prime)
 print sum
