e7bbede94ba04cc28dee9429826191a7c5fa244e
Mo Chen <kidrock2006@gmail.com>
2014-03-12 14:47:54 -0400

    Update
    1. Modified Euler 68

--- Magic 5-gon ring.py
+++ Magic 5-gon ring.py
@@ -6,33 +6,33 @@
 import itertools
 
 def check_rings(outer, inner):
-    sm = [0] * len(inner)
+    sm=[0] * len(inner)
     for a in range(len(inner)):
         for b in range(len(outer)):
-            sm[b] = outer[b] + inner[(a+b-1)%len(inner)] + inner[(a+b)%len(inner)]
-        av = sum(sm) / len(sm)
-        if all([s == av for s in sm]):
+            sm[b]=outer[b]+inner[(a+b-1)%len(inner)]+inner[(a+b)%len(inner)]
+        av=sum(sm)/len(sm)
+        if all([s==av for s in sm]):
             return a
     return -1
 
 
-start = [6]
-outer = [7, 8, 9, 10]
-inner = range(1,6)
+start=[6]
+outer=[7, 8, 9, 10]
+inner=range(1,6)
 
-outer_rings = []
+outer_rings=[]
 for p in itertools.permutations(outer):
-    outer_rings.append(start + list(p))
-inner_rings = [p for p in itertools.permutations(inner)]
-answers = []
+    outer_rings.append(start+list(p))
+inner_rings=[p for p in itertools.permutations(inner)]
+answers=[]
 for ou in outer_rings:
     for inn in inner_rings:
-        start_index = check_rings(ou, inn)
-        if start_index >= 0:
-            ds = ""
+        start_index=check_rings(ou, inn)
+        if start_index>=0:
+            ds=""
             for i in range(len(ou)):
-                ds = ds + str(ou[i])
+                ds=ds+str(ou[i])
                 for j in range(2):
-                    ds = ds + str(inn[(i+j-1)%len(inner)])
+                    ds=ds+str(inn[(i+j-1)%len(inner)])
             answers.append(int(ds))
 print max(answers)
